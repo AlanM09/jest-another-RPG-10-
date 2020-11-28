6012866ddad68332c219faa53a4a0b8c45150e56
Your Name <alanmayer@email.arizona.edu>
2020-11-27 16:30:19 -0800

    sec 2

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,6 +1,10 @@
 const { test, expect } = require('@jest/globals');
 const Player = require('../lib/Player');
+const Potion = require('../lib/Potion');
 
+jest.mock('../lib/Potion');
+
+console.log(new Potion());
 
 
 test('creates a player object', () => {
@@ -10,7 +14,4 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
-    expect(player.inventory).toEqual(
-        expect.arrayContaining([expect.any(Object)])
-      );
 });
